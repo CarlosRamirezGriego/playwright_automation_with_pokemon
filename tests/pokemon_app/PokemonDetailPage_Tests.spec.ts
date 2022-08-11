90bb1978e22fcdc5075b899e7a9fc9006bb10cb1
CarlosRG <cramirez.griego@gmail.com>
2022-08-11 16:55:33 -0700

    Added API test example

--- tests/pokemon_app/PokemonDetailPage_Tests.spec.ts
+++ tests/pokemon_app/PokemonDetailPage_Tests.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { MainPageClass } from "../../business_logic/pokemon_app/ui/MainPage";
 import { NationalDexPageClass } from "../../business_logic/pokemon_app/ui/NationalDexPage";
+import { ApiPokemonLogic } from "../../business_logic/pokemon_app/api/Pokemon"
 import { CommonClass } from "../../common/pokemon_app/CommonUI";
 
 
@@ -16,5 +17,15 @@ test('Load a Pokemon Detail Page @e2e  @test001', async ({ page }) => {
   console.log(await natPage.returnNumberOfPokemonWithThisName("Pikachu"))
   await natPage.clickPokemonWithThisName("Pikachu")
   await commonPage.sleep(5000)
+
+});
+
+
+test('Using API to get information  @e2e  @test002', async ({ }) => {
+  //Required Classes initialization
+  const pkAPILogic = new ApiPokemonLogic()
+
+  console.log(await pkAPILogic.returnNameOfPokemonWithThisNumber(25555))
+  console.log(await pkAPILogic.returnNumberOfPokemonWithThisName("Pikachuuu"))
 
 });
